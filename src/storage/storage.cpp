bb058d06bdedaae42f94dd964c75ec8a63dc7781
Paul Hamilton <phamtec@mac.com>
2024-06-21 16:42:10 +1000

    Production DB.

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -26,7 +26,9 @@ Storage::Storage() {
 StorageImpl::StorageImpl() {
 
   _instance.reset(new mongocxx::instance());
-  mongocxx::uri uri("mongodb://127.0.0.1:27017");
+ 
+//  mongocxx::uri uri("mongodb://127.0.0.1:27017");
+  mongocxx::uri uri(" mongodb://fiveEstellas:visualops@localhost/fiveEstellas");
   _client.reset(new mongocxx::client(uri));
   _db = (*_client)["dev"];
 
