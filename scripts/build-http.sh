e67faf81a3c70805783c3a5fa1ac4d990c11272e
Paul Hamilton <phamtec@mac.com>
2024-07-10 17:01:42 +1000

    Script for building http stuff.

---
+++ scripts/build-http.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 
+# Build the project for http access
+
+if [ ! -d "zmqchat" ];
+then
+  echo "zmqchat project missing."
+  exit 1
+fi
+
+pushd zmqchat/angular/login-ui
+npm install
+./build.sh
+popd
+
+pushd zmqchat/angular/admin-ui
+npm install
+./build.sh
+popd
