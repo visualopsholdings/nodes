381867c264b24784f4465300fb8ad75040bd8dc9
Paul Hamilton <phamtec@mac.com>
2024-08-01 17:15:25 +1000

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -53,7 +53,7 @@ void Server::run() {
   const std::chrono::milliseconds timeout{500};
   while (1) {
   
-    BOOST_LOG_TRIVIAL(debug) << "polling for messages";
+//    BOOST_LOG_TRIVIAL(debug) << "polling for messages";
     zmq::message_t message;
     zmq::poll(&items[0], 1, timeout);
   
