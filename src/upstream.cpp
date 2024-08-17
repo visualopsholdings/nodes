23a2fd4bd5d2cc3daffcf2dfbe12931260fd0de3
Paul Hamilton <phamtec@mac.com>
2024-08-17 18:22:04 +1000

    Update upstream.cpp

--- src/upstream.cpp
+++ src/upstream.cpp
@@ -24,9 +24,9 @@ Upstream::Upstream(Server *_server, zmq::context_t &context, int type, const str
   setsockopt(ZMQ_CURVE_SECRETKEY, privateKey.c_str(), privateKey.size());
   setsockopt(ZMQ_CURVE_PUBLICKEY, pubKey.c_str(), pubKey.size());
 #else
-  _sub->set(zmq::sockopt::curve_serverkey, upstreamPubKey.c_str(), upstreamPubKey.size());
-  _sub->set(zmq::sockopt::curve_secretkey, privateKey.c_str(), privateKey.size());
-  _sub->set(zmq::sockopt::curve_publickey, pubKey.c_str(), pubKey.size());
+  set(zmq::sockopt::curve_serverkey, upstreamPubKey.c_str(), upstreamPubKey.size());
+  set(zmq::sockopt::curve_secretkey, privateKey.c_str(), privateKey.size());
+  set(zmq::sockopt::curve_publickey, pubKey.c_str(), pubKey.size());
 #endif
 
   string url = "tcp://" + upstream + ":" + to_string(port);
